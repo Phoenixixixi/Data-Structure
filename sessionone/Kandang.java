7606d34b958ade0cc3b7708e2b48b98bc13c1a44
phoenixixixi <mhammmaddustin@gmail.com>
2026-06-13 20:42:54 +0700

    session 3

---
+++ sessionone/Kandang.java
@@ -0,0 +1,19 @@
+package sessionone;
+
+public class Kandang {
+
+    public void registKandang(String jenis){
+        System.out.println("Jenis hewan: " + jenis);
+    }
+
+    public void registKandang(String jenis, int jumlah){
+        System.out.println("Jenis hewan: " + jenis);
+        System.out.println("Jumlah: " + jumlah);
+    }
+
+    public void registKandang(String jenis, int jumlah, String lokasi){
+        System.out.println("Jenis hewan: " + jenis);
+        System.out.println("Jumlah: " + jumlah);
+        System.out.println("Lokasi: " + lokasi);
+    }
+}
---
+++ sessionone/Main.java
@@ -0,0 +1,11 @@
+package sessionone;
+
+public class Main {
+    
+    public static void main(String[] args){
+        Kandang kambing = new Kandang();
+
+        kambing.registKandang("kambing", 20);
+
+    }
+}
